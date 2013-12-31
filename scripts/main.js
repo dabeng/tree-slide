556cac5a7503e175fd91ed097054b9e456feb200
Xuebin Dong <xudong@redhat.com>
2013-12-31 17:16:35 +0800

    add turning page effect

--- scripts/main.js
+++ scripts/main.js
@@ -41,25 +41,44 @@
    });
 
     // left, top, right and down keys
-    $(document).on('keypress', function(event) {
-      switch(event.which)
-      {
-        case 37:
-          turnToPreviousSlide();
-          break;
-        case 38:
-          break;
-        case 39: {
+    $(document).on('keyup', function(event) {
+      switch(event.which) {
+        case 37: {
           if($('#container').is('.multipleSlide')) {
-
+            focusLeftSlide();
           }
           else if($('#container').is('.singleSlide')) {
-          	turnToNextSlide();
+            turnToNewSlide('previous');
           }
           break;
         }
-        case 40:
+        case 38: {
+          if($('#container').is('.multipleSlide')) {
+            focusUpSlide();
+          }
+          else if($('#container').is('.singleSlide')) {
+            turnToNewSlide('previous');
+          }
           break;
+        }
+        case 39: {
+          if($('#container').is('.multipleSlide')) {
+            focusRightSlide();
+          }
+          else if($('#container').is('.singleSlide')) {
+          	turnToNewSlide('next');
+          }
+          break;
+        }
+        case 40: {
+          if($('#container').is('.multipleSlide')) {
+            focusDownSlide();
+          }
+          else if($('#container').is('.singleSlide')) {
+            turnToNewSlide('next');
+          }
+          break;
+        }
       }
     });
 
@@ -147,8 +166,8 @@
 	controls.maxDistance = 6000;
 	controls.addEventListener('change', render);
 
-	$('#effect-3dtable').on('click', function (event) { transform( targets.table, 1000 ); });
-	$('#effect-sphere').on('click', function (event) { transform( targets.sphere, 1000 ); });
+	$('#effect-3dtable').on('click', function (event) { transform(targets.table, 1000 ); });
+	$('#effect-sphere').on('click', function (event) { transform(targets.sphere, 1000 ); });
 
 	transform(targets.table, 1000);
 
@@ -196,12 +215,22 @@ function render() {
 
 function makeCenterEffect(index) {
   return function () {
+    //
+    controls.removeEventListener('change', render);
+
   	var jSelectedSlide = $(objects[index].element);
   	var jOtherSlides = $('.slide').not(objects[index].element);
+
+    // set the current view mode to sigleSlide
+    $('#container').removeClass();
+    $('#container').addClass('singleSlide');
     
-    // set flag
+    // set flag to identify which slide is being read
     jOtherSlides.removeClass('currentSlide');
     jSelectedSlide.addClass('currentSlide');
+
+    // create the data-index property of slide of jquery object
+    $('.currentSlide').prop('data-index', index);
 
     // hide other slides
     jOtherSlides.addClass('hidden');
@@ -213,15 +242,103 @@ function makeCenterEffect(index) {
   	// turn to the screen
   	controls.reset();
     var vector = new THREE.Vector3(0, 0, cameraDist);
-	objects[index].lookAt(vector);
+	  objects[index].lookAt(vector);
 
     // fit into render area by HEIGHT
     $(objects[index].element).css({'width': '800px', 'height': '600px'});
 
-	objects[index].translateZ(cameraDist - ((window.innerHeight/2) / Math.tan(fov / 2 * pi)));
+	  objects[index].translateZ(cameraDist - ((window.innerHeight / 2) / Math.tan(fov / 2 * pi)));
 
-
-	// re-render the scene
-	render();
+	  // re-render the scene
+	  render();
   };
+}
+
+// turn to new slide according to the current slide
+function turnToNewSlide(type) {
+  var index = $('.currentSlide').prop('data-index');
+  // determine whether it can be turned to the new slide
+  if(type === 'next') {
+    if(index >= objects.length - 1) {
+      return;
+    }
+  } else {
+    if(index === 0) {
+      return;
+    }
+  }
+
+  // hide the current slide
+  hideCurrentSlide(index, 500);
+
+  // show the new slide
+  if(type === 'next') {
+    showNewSlide(index + 1, 500);
+  } else {
+    showNewSlide(index - 1, 500);
+  }
+}
+
+
+function hideCurrentSlide(index, duration) {
+  var object = objects[index];
+  var target = targets.table[index];
+  // TWEEN.removeAll();
+
+  new TWEEN.Tween(object.position).to({x: target.position.x, y: target.position.y,
+    z: target.position.z}, Math.random() * duration + duration)
+    .easing(TWEEN.Easing.Exponential.InOut).start();
+
+  new TWEEN.Tween(object.rotation).to({x: target.rotation.x, y: target.rotation.y,
+    z: target.rotation.z}, Math.random() * duration + duration)
+    .easing(TWEEN.Easing.Exponential.InOut).start();
+  
+
+  new TWEEN.Tween(window).to({}, duration * 2).onUpdate(render).start();
+}
+
+function showNewSlide(index, duration) {
+  var jSelectedSlide = $(objects[index].element);
+  var jOtherSlides = $('.slide').not(objects[index].element);
+    
+  // set flag to identify which slide is being read
+  jOtherSlides.removeClass('currentSlide');
+  jSelectedSlide.addClass('currentSlide');
+
+  // create the data-index property of slide of jquery object
+  $('.currentSlide').prop('data-index', index);
+
+  // show the focused slide and hide the other slides
+  jSelectedSlide.removeClass('hidden');
+  jOtherSlides.addClass('hidden');
+
+  // move to the origin
+  // objects[index].translateX(-targets.table[index].position.x);
+  // objects[index].translateY(-targets.table[index].position.y);
+
+  // turn to the screen
+  // controls.reset();
+  // var vector = new THREE.Vector3(0, 0, cameraDist);
+  // objects[index].lookAt(vector);
+
+  // fit into render area by HEIGHT
+  $(objects[index].element).css({'width': '800px', 'height': '600px'});
+  // objects[index].translateZ(cameraDist - ((window.innerHeight / 2) / Math.tan(fov / 2 * pi)));
+
+  // re-render the scene
+  // render();
+
+    var object = objects[index];
+  var target = targets.table[index];
+
+    new TWEEN.Tween(object.position).to({x: 0, y: 0,
+    z: cameraDist - (window.innerHeight / 2) / Math.tan(fov / 2 * pi)}, Math.random() * duration + duration)
+    .easing(TWEEN.Easing.Exponential.InOut).start();
+
+  new TWEEN.Tween(object.rotation).to({x: target.rotation.x, y: target.rotation.y,
+    z: target.rotation.z}, Math.random() * duration + duration)
+    .easing(TWEEN.Easing.Exponential.InOut).start();
+
+
+  new TWEEN.Tween(window).to({}, duration).onUpdate(render).start();
 }
