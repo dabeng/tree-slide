f0384febe3ef1e959126cb2d939d0ff7b9157502
dabeng <xinguan413@163.com>
2014-01-05 08:28:49 +0800

    apply $.when().done() to the call to the setRowCol function

--- scripts/main.js
+++ scripts/main.js
@@ -1,131 +1,149 @@
-  var camera, scene, renderer;
-  var cameraDist = 3000;
-  var fov = 40;// field of view
-  var pi = Math.PI / 180;
-  var controls;
-  var objects = [];
-  var targets = { 'table': [], 'sphere': [], 'helix': [], 'grid': [] };
+var camera, scene, renderer;
+var cameraDist = 3000;
+var fov = 40;// field of view
+var pi = Math.PI / 180;
+var controls;
+var objects = [];
+var targets = {'table': [], 'sphere': [], 'helix': [], 'grid': []};
 
-  $(function($){
+$(function($){
 	
-    init();
-    animate();
+  init();
+  animate();
 
-  });
+});
 
 function init() {
-    var aspect = window.innerWidth / window.innerHeight;
-	  camera = new THREE.PerspectiveCamera(fov, aspect, 0, 10000);
+  // first of all, append required properties to the datasource of slides
+  $.when(setRowCol(datasource)).done(generate3DEffects);
 
-	  camera.position.z = cameraDist;
-	  scene = new THREE.Scene();
-    var vector = new THREE.Vector3();
-
-    var nearPlaneHeight = cameraDist * Math.tan(fov / 2 * pi) * 2;
-    var nearPlaneWidth = nearPlaneHeight * (4 / 3);
-    var margin = 20;
-    setRowCol(datasource);
-
-    var rowColNum = getRowCol(datasource.length);
-    var rowCount = rowColNum.row;
-	  var rowHeight = Math.round((nearPlaneHeight - (rowCount - 1) * margin) / rowCount);
-	  var colCount = rowColNum.col;
-    var colWidth = Math.round((nearPlaneWidth - (colCount - 1) * margin) / colCount);
-    var originalSize = {'width': colWidth, 'height': rowHeight};
-
-	// 3D table
-	$.each(datasource, function(index, slide) {
-	  var jSlide = $('<article>', { 'class': 'slide', 'css':{
-	  	'width': colWidth,
-	  	'height': rowHeight,
-	  	'backgroundColor': 'rgba(51,0,51,' + (Math.random() * 0.5 + 0.25) + ')'}
-	  });
-	  jSlide.append($('<div>', { 'class': 'number', 'text': index + 1 }));
-	  jSlide.append($('<div>', { 'class': 'banner', 'text': slide['name'] }));
-	  jSlide.append($('<div>', { 'class': 'content'}).html(slide['fullname']));
-
-	  jSlide.on('dblclick', makeCenterEffect(index, 500, originalSize));
-
-	  var css3dObject = new THREE.CSS3DObject(jSlide[0]);
-	  css3dObject.position.x = Math.random() * 4000 - 2000;
-	  css3dObject.position.y = Math.random() * 4000 - 2000;
-	  css3dObject.position.z = Math.random() * 4000 - 2000;
-	  scene.add(css3dObject);
-	  objects.push(css3dObject);
-
-      var object3D = new THREE.Object3D();
-	  object3D.position.x = -(colCount * (colWidth + margin) - margin) / 2
-	    + (slide['col'] - 1) * (colWidth + margin) + colWidth / 2;
-	  object3D.position.y = (rowCount * (rowHeight + margin) - margin) / 2
-	    - (slide['row'] - 1) * (rowHeight + margin) - rowHeight / 2; 
-	
-	  targets.table.push(object3D);
-    });
-
-
-    var count = objects.length;
-    var phi, theta;
-
-	// sphere
-	var diameter = Math.round(nearPlaneWidth/Math.PI);
-	for (i = 0; i < count; i++) {
-	  var object3D = new THREE.Object3D();
-	  phi = Math.acos(-1 + ( 2 * i ) / count);
-	  theta = Math.sqrt(count * Math.PI) * phi;
-	  object3D.position.x = diameter * Math.cos(theta) * Math.sin(phi);
-	  object3D.position.y = diameter * Math.sin(theta) * Math.sin(phi);
-	  object3D.position.z = diameter * Math.cos(phi);
-
-	  vector.copy(object3D.position).multiplyScalar(2);
-	  object3D.lookAt(vector);
-	  targets.sphere.push(object3D);
-	}
-
-	//
-	renderer = new THREE.CSS3DRenderer();
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	renderer.domElement.style.position = 'absolute';
-	$('#container').append(renderer.domElement);
-
-	//
-	controls = new THREE.TrackballControls(camera, renderer.domElement);
-	controls.rotateSpeed = 0.5;
-	controls.minDistance = 500;
-	controls.maxDistance = 6000;
-	controls.addEventListener('change', render);
-
-	$('#effect-3dtable').on('click', function (event) { transform(targets.table, 1000 ); });
-	$('#effect-sphere').on('click', function (event) { transform(targets.sphere, 1000 ); });
-
-	transform(targets.table, 1000);
-
-	//
-	$(window).on('resize', onWindowResize);
-
-  // layout
-  $('#effect').on('mouseenter', function(){
+  // bind event handlers for effect configuration panel
+  $('#effect').on('mouseenter', function() {
     if(!$(this).is(':animated')) {
       $(this).animate({'right': '0px'}, 'slow');
     }
   });
-
-  $('#effect').on('mouseleave', function(){
+  $('#effect').on('mouseleave', function() {
     if(!$(this).is(':animated')) {
       $(this).animate({'right': '-190px'}, 'slow');
     }
   });
-
-  $('#catalogue').on('mouseenter', function(){
+  
+  // bind event handlers for the catalogue panel of slides
+  $('#catalogue').on('mouseenter', function() {
     if(!$(this).is(':animated')) {
       $(this).animate({'left': '0px'}, 'slow');
     }
   });
-
-  $('#catalogue').on('mouseleave', function(){
+  $('#catalogue').on('mouseleave', function() {
     if(!$(this).is(':animated')) {
       $(this).animate({'left': '-190px'}, 'slow');
     }
   });
+
+  // animation effect of navigation panel
+  var animationEndHandler= {
+    'webkitAnimationEnd': function() {$(this).css('-webkit-animation-play-state', 'paused');},
+    'animationEnd' :function() {$(this).css('animation-play-state', 'paused');}
+  };
+  $('#triangle-left-effect').on(animationEndHandler);
+  $('#triangle-up-effect').on(animationEndHandler);
+  $('#triangle-right-effect').on(animationEndHandler);
+  $('#triangle-down-effect').on(animationEndHandler);
+
+}
+
+/*
+ * generate 3D effects based on three.js
+*/
+function generate3DEffects() {
+  var aspect = window.innerWidth / window.innerHeight;
+  camera = new THREE.PerspectiveCamera(fov, aspect, 0, 10000);
+
+  camera.position.z = cameraDist;
+  scene = new THREE.Scene();
+  var vector = new THREE.Vector3();
+
+  var nearPlaneHeight = cameraDist * Math.tan(fov / 2 * pi) * 2;
+  var nearPlaneWidth = nearPlaneHeight * (4 / 3);
+  var margin = 20
+
+
+  var rowColNum = getRowCol(datasource.length);
+  var rowCount = rowColNum.row;
+  var rowHeight = Math.round((nearPlaneHeight - (rowCount - 1) * margin) / rowCount);
+  var colCount = rowColNum.col;
+  var colWidth = Math.round((nearPlaneWidth - (colCount - 1) * margin) / colCount);
+  var originalSize = {'width': colWidth, 'height': rowHeight};
+
+  // 3D table
+  $.each(datasource, function(index, slide) {
+    var jSlide = $('<article>', { 'class': 'slide', 'css':{
+      'width': colWidth,
+      'height': rowHeight,
+      'backgroundColor': 'rgba(51,0,51,' + (Math.random() * 0.5 + 0.25) + ')'}
+    });
+    jSlide.append($('<div>', { 'class': 'number', 'text': index + 1 }));
+    jSlide.append($('<div>', { 'class': 'banner', 'text': slide['banner'] }));
+    jSlide.append($('<div>', { 'class': 'content'}).html(slide['content']));
+
+    jSlide.on('dblclick', makeCenterEffect(index, 500, originalSize));
+
+    var css3dObject = new THREE.CSS3DObject(jSlide[0]);
+    css3dObject.position.x = Math.random() * 4000 - 2000;
+    css3dObject.position.y = Math.random() * 4000 - 2000;
+    css3dObject.position.z = Math.random() * 4000 - 2000;
+    scene.add(css3dObject);
+    objects.push(css3dObject);
+
+    var object3D = new THREE.Object3D();
+    object3D.position.x = -(colCount * (colWidth + margin) - margin) / 2
+      + (slide['col'] - 1) * (colWidth + margin) + colWidth / 2;
+    object3D.position.y = (rowCount * (rowHeight + margin) - margin) / 2
+      - (slide['row'] - 1) * (rowHeight + margin) - rowHeight / 2; 
+  
+    targets.table.push(object3D);
+  });
+
+
+  var count = objects.length;
+  var phi, theta;
+
+  // sphere
+  var diameter = Math.round(nearPlaneWidth/Math.PI);
+  for (i = 0; i < count; i++) {
+    var object3D = new THREE.Object3D();
+    phi = Math.acos(-1 + ( 2 * i ) / count);
+    theta = Math.sqrt(count * Math.PI) * phi;
+    object3D.position.x = diameter * Math.cos(theta) * Math.sin(phi);
+    object3D.position.y = diameter * Math.sin(theta) * Math.sin(phi);
+    object3D.position.z = diameter * Math.cos(phi);
+
+    vector.copy(object3D.position).multiplyScalar(2);
+    object3D.lookAt(vector);
+    targets.sphere.push(object3D);
+  }
+
+  //
+  renderer = new THREE.CSS3DRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.domElement.style.position = 'absolute';
+  $('#container').append(renderer.domElement);
+
+  //
+  controls = new THREE.TrackballControls(camera, renderer.domElement);
+  controls.rotateSpeed = 0.5;
+  controls.minDistance = 500;
+  controls.maxDistance = 6000;
+  controls.addEventListener('change', render);
+
+  $('#effect-3dtable').on('click', function (event) { transform(targets.table, 1000 ); });
+  $('#effect-sphere').on('click', function (event) { transform(targets.sphere, 1000 ); });
+
+  transform(targets.table, 1000);
+
+  //
+  $(window).on('resize', onWindowResize);
 
   // left, top, right and down keys
   $(document).on('keyup', function(event) {
@@ -178,17 +196,6 @@ function init() {
       }
     }
   });
-
-  // animation effect of navigation panel
-  var animationEndHandler= {'webkitAnimationEnd': function() {$(this).css('-webkit-animation-play-state', 'paused');},
-    'animationEnd' :function() {$(this).css('animation-play-state', 'paused');}};
-  $('#triangle-left-effect').on(animationEndHandler);
-  $('#triangle-up-effect').on(animationEndHandler);
-  $('#triangle-right-effect').on(animationEndHandler);
-  $('#triangle-down-effect').on(animationEndHandler);
-
-  
-
 }
 
 function transform(targets, duration) {
