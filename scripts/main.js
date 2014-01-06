ae979ef883958c27f9e3126c38294cd0a0048a08
Xuebin Dong <xudong@redhat.com>
2014-01-06 16:24:13 +0800

    add generate3DPosition() to produce the position for every slide

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,8 @@ var cameraDist = 3000;
 var fov = 40;// field of view
 var pi = Math.PI / 180;
 var controls;
-var objects = [];
-var targets = {'table': [], 'sphere': [], 'helix': [], 'grid': []};
+var objects = {};
+var targets = {};
 
 $(function($){
 	
@@ -196,6 +196,63 @@ function generate3DEffects() {
       }
     }
   });
+}
+
+function generate3DPosition(arr, parentId) {
+  objects[parentId] = [];
+  targets[parentId] = {"table": [], "sphere": []};
+
+  // 3D table
+  $.each(datasource, function(index, slide) {
+    var jSlide = $('<article>', { 'class': 'slide', 'css':{
+      'width': colWidth,
+      'height': rowHeight,
+      'backgroundColor': 'rgba(51,0,51,' + (Math.random() * 0.5 + 0.25) + ')'}
+    });
+    // jSlide.uniqueId();
+    jSlide.append($('<div>', { 'class': 'number', 'text': index + 1 }));
+    jSlide.append($('<div>', { 'class': 'banner', 'text': slide['banner'] }));
+    jSlide.append($('<div>', { 'class': 'content'}).html(slide['content']));
+
+    jSlide.on('dblclick', makeCenterEffect(index, 500, originalSize));
+
+    var css3dObject = new THREE.CSS3DObject(jSlide[0]);
+    css3dObject.position.x = Math.random() * 4000 - 2000;
+    css3dObject.position.y = Math.random() * 4000 - 2000;
+    css3dObject.position.z = Math.random() * 4000 - 2000;
+    scene.add(css3dObject);
+
+    objects[parentId].push(css3dObject);
+
+    var object3D = new THREE.Object3D();
+    object3D.position.x = -(colCount * (colWidth + margin) - margin) / 2
+      + (slide['col'] - 1) * (colWidth + margin) + colWidth / 2;
+    object3D.position.y = (rowCount * (rowHeight + margin) - margin) / 2
+      - (slide['row'] - 1) * (rowHeight + margin) - rowHeight / 2; 
+    targets[parentId].table.push(object3D);
+  });
+
+
+  var count = objects.length;
+  var phi, theta;
+
+  // sphere
+  var diameter = Math.round(nearPlaneWidth/Math.PI);
+  var vector = new THREE.Vector3();
+  $.each(arr, function(index, slide) {
+    var object3D = new THREE.Object3D();
+    phi = Math.acos(-1 + ( 2 * index ) / count);
+    theta = Math.sqrt(count * Math.PI) * phi;
+    object3D.position.x = diameter * Math.cos(theta) * Math.sin(phi);
+    object3D.position.y = diameter * Math.sin(theta) * Math.sin(phi);
+    object3D.position.z = diameter * Math.cos(phi);
+
+    vector.copy(object3D.position).multiplyScalar(2);
+    object3D.lookAt(vector);
+
+    targets[parentId].sphere.push(object3D);
+  }
+
 }
 
 function transform(targets, duration) {
